bd234e10cba0407e1b5e2825fc79cd2c07c8ade0
Mikhail Tyurin <tyurin@tektorg.ru>
2018-12-14 13:24:57 +0300

    Поправил по кривой вывод kesl

--- av_server.go
+++ av_server.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
-	. "github.com/blacked/go-zabbix"
+	zabbix "github.com/blacked/go-zabbix"
 	"io/ioutil"
 	"log"
 	"log/syslog"
@@ -30,10 +30,10 @@ func sendToZabbix(checkDuration int64) {
 	strDuration := strconv.FormatInt(checkDuration, 10)
 	hostname := strings.Split(fqdn, ".")[0]
 	log.Printf("sending metrics to zabbix server %s : %s", defaultHost, string(strDuration))
-	var metrics []*Metric
-	metrics = append(metrics, NewMetric(hostname, "avcheck.duration", string(strDuration), time.Now().Unix()))
-	packet := NewPacket(metrics)
-	z := NewSender(defaultHost, defaultPort)
+	var metrics []*zabbix.Metric
+	metrics = append(metrics, zabbix.NewMetric(hostname, "avcheck.duration", string(strDuration), time.Now().Unix()))
+	packet := zabbix.NewPacket(metrics)
+	z := zabbix.NewSender(defaultHost, defaultPort)
 	resp, err := z.Send(packet)
 	if err != nil {
 		log.Println("unable to send data to zabbix server" + err.Error())
@@ -78,7 +78,8 @@ func handleConection(conn net.Conn) {
 	textReader := textproto.NewReader(reader)
 	filename, _ := textReader.ReadLine()
 	fmt.Println(filename)
-	cmd := exec.Command("/opt/kaspersky/kesl/bin/kesl-control", "--action", "Remove", "--scan-file", filename)
+	cmdLine := "/opt/kaspersky/kesl/bin/kesl-control --scan-file " + filename + "  --action Remove  | iconv -f iso-8859-5 -t utf-8"
+	cmd := exec.Command("/bin/bash", "-c", cmdLine)
 	var stderr bytes.Buffer
 	var stdout bytes.Buffer
 	cmd.Stderr = &stderr
